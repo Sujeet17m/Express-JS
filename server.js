baa13c0bb73a491bcde6a72f933e658131c8fd5b
Sujeet17m <sujeet17das@gmail.com>
2025-12-26 01:33:23 +0530

    req.params

--- server.js
+++ server.js
@@ -13,7 +13,37 @@ const PORT = 7000;
 const app = express();
 
 app.get('/api', (req, res)=> {
-    res.json(startups);
+    let filteredData = startups;
+
+    if(req.query.industry){
+        filteredData = filteredData.filter(startup => 
+            startup.industry.toLowerCase() === req.query.industry.toLowerCase()
+        );
+    }
+    if(req.query.country){
+        filteredData = filteredData.filter(startup => 
+            startup.country.toLowerCase() === req.query.country.toLowerCase()
+        );
+    }
+    if(req.query.continent){
+        filteredData = filteredData.filter(startup => 
+            startup.continent.toLowerCase() === req.query.continent.toLowerCase()
+        );
+    }
+    if(req.query.is_seeking_funding){
+        const isSeekingFunding = req.query.is_seeking_funding.toLowerCase() === 'true';
+        filteredData = filteredData.filter(startup => 
+            startup.is_seeking_funding === isSeekingFunding
+        );
+    }
+    if(req.query.has_mvp){
+        const hasMvp = req.query.has_mvp.toLowerCase() === 'true';
+        filteredData = filteredData.filter(startup => 
+            startup.has_mvp === hasMvp
+        );
+    }
+
+    res.json(filteredData);
     console.log(req.query);
     
 })
